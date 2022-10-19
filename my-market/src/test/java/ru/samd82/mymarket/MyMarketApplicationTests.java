3fdaeb0e9af9e7b8d227303252605c87ede558ff
samd82GB <samd82@yandex.ru>
2022-10-19 12:46:51 +0500

    Initial commit

---
+++ my-market/src/test/java/ru/samd82/mymarket/MyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.samd82.mymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
