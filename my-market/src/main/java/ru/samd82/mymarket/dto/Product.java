9eeec3be507ab1c18f9ed385b1596f759cd239a4
samd82GB <samd82@yandex.ru>
2022-10-20 12:52:40 +0500

    Выполнено ДЗ к уроку 3

---
+++ my-market/src/main/java/ru/samd82/mymarket/dto/Product.java
@@ -0,0 +1,17 @@
+package ru.samd82.mymarket.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    private Long id;
+    private String title;
+    private float cost;
+
+
+}
---
+++ my-market/src/main/java/ru/samd82/mymarket/repo/ProductRepository.java
@@ -0,0 +1,34 @@
+package ru.samd82.mymarket.repo;
+
+import org.springframework.stereotype.Repository;
+import ru.samd82.mymarket.dto.Product;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public class ProductRepository {
+
+    private List<Product> products;
+
+    @PostConstruct
+    public void init() {
+        products = new ArrayList<>(Arrays.asList(
+                new Product(1L, "Smartphone", 15000),
+                new Product(2L, "Fax", 25000),
+                new Product(3L, "PC", 35000)
+        ));
+    }
+
+    public Product findById(Long id) {
+        return products.stream().filter(p -> p.getId().equals(id)).findFirst()
+                .orElseThrow(() -> new RuntimeException("Продукт не найден"));
+    }
+
+    public List<Product> getProducts(){
+        return products;
+    }
+
+}
